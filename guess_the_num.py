8557bfac97ef6f4926720a99a81210e4eb2b821d
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-26 18:01:59 +0200

    added if statment to give feedback on number to high or to low. Return message with number if correct. Added welcome message. main function for future development

--- guess_the_num.py
+++ guess_the_num.py
@@ -14,7 +14,23 @@ def guess(x):
 
     # While users guess is not the random number keep looping till user guess corectally
     while guess != random_number: 
-        guess = input(f"Guess a number between 1 and {x}: ")
-        print(guess)
+        # Ask user to guess and convert to integer
+        guess = int(input(f"Guess a number between 1 and {x}: ")) 
+        
+        # Check if the user's guess is too low, high or correct
+        if guess < random_number: # if user guess too low
+            print("Sorry, guess again. Too Low!")
+        elif guess > random_number: # if user guess too high
+            print("Sorry, guess again. Too High!")
+        
+    # If the user guess correctly, break the loop and print congrats message
+    print(f"Yay, congrats. You have guessed the number {random_number} correctly!")
 
-guess(10)        
+def main():
+    print("Welcome to the Guess the Number Game!\n")
+    print("I am thinking of a number between 1 and 10.")
+    guess(10)
+
+
+main() # Call the main function - to Start the game
+      
