551a58f07316ed568768ba0718fba281d73d0b4c
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-26 18:24:36 +0200

    Added dif levels to make the game more fun and challenging. Changed layout to improve user experience

--- guess_the_num.py
+++ guess_the_num.py
@@ -2,6 +2,9 @@
 Guess the Number Game
 This is a simple command-line game where the user/player tries to guess a randomly generated number within a certain range.
 A game where you need to guess a randomly generated number.
+
+This computer holds a secret number and you need to guess it.
+You can choose the difficulty level to make it more fun and challenging.
 """
 
 import random # Import the random module to generate random numbers
@@ -15,7 +18,7 @@ def guess(x):
     # While users guess is not the random number keep looping till user guess corectally
     while guess != random_number: 
         # Ask user to guess and convert to integer
-        guess = int(input(f"Guess a number between 1 and {x}: ")) 
+        guess = int(input(f"Guess a number: ")) 
         
         # Check if the user's guess is too low, high or correct
         if guess < random_number: # if user guess too low
@@ -27,9 +30,27 @@ def guess(x):
     print(f"Yay, congrats. You have guessed the number {random_number} correctly!")
 
 def main():
-    print("Welcome to the Guess the Number Game!\n")
-    print("I am thinking of a number between 1 and 10.")
-    guess(10)
+    # Welcome message and instructions for the game
+    print("\n --- Welcome to Guess the Number Game! ---\n")
+    print("This computer holds a secret number and you need to guess it.\n")
+    print("Difficulty Level:\n" \
+        "1. Easy - between 1 and 10\n" \
+        "2. Medium - between 1 and 50\n" \
+        "3. Hard - between 1 and 100\n")
+
+    # Ask user to choose the game difficulty level
+    difficulty = int(input("Choose difficulty Level (1-3): "))
+    print()
+    
+    if difficulty == 1: # Easy level - between 1 and 10
+        print("I am thinking of a number between 1 and 10.")
+        guess(10)
+    elif difficulty == 2: # Medium level - between 1 and 50
+        print("I am thinking of a number between 1 and 50.")
+        guess(50)
+    elif difficulty == 3: # Hard level - between 1 and 100
+        print("I am thinking of a number between 1 and 100.")
+        guess(100)
 
 
 main() # Call the main function - to Start the game
