c602bc621de87aa2d8723d21efdb20d212cae612
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-26 17:32:42 +0200

    Start project...

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Guess_the_Number
+# Guess_the_Number 
+
+A guessing game where the computer has a secret number
+And we are trying to guess that secret number
+
+Step 1: Having the computer generate a secret number for us to guess
---
+++ guess_the_num.py
@@ -0,0 +1,5 @@
+"""
+Guess the Number Game
+This is a simple command-line game where the user/player tries to guess a randomly generated number within a certain range.
+A game where you need to guess a randomly generated number.
+"""
